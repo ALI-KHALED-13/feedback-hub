fa90f55a212810aaaaedb73612324da06b7619ea
Ali <aliknake@gmail.com>
2023-03-03 15:00:53 +0200

    wrapping everything in the new catch all error handler

--- api/tags/controllers.js
+++ api/tags/controllers.js
@@ -7,15 +7,12 @@ const getAllTags = async (req, res)=> {
 
 const addTags = async (req, res)=> { // two accepted formulas {name: "lena"} || ["lena", "mop"]
   const tagsToBeAdded = Object.values(req.body);
-  try {
-    for (let tagName of tagsToBeAdded){
-      const tag = await Tag.create({name: tagName})
-      tag.save();
-    }
-    res.status(200).json("tag(s) added successfully")
-  } catch (err) {
-    res.status(400).json({message: err.message})
+
+  for (let tagName of tagsToBeAdded){
+    const tag = await Tag.create({name: tagName})
+    tag.save();
   }
+  res.status(200).json("tag(s) added successfully")
 }
 
 
--- api/tags/routes.js
+++ api/tags/routes.js
@@ -1,13 +1,14 @@
 const express = require("express");
+const { catchErrors } = require("../utils/handleErrors");
 const router = express.Router();
 
 const {getAllTags, addTags} = require("./controllers");
 
 // upon adding tag that react comp should know what's available in the db already to suggest it as user types
-router.get("/", getAllTags);
+router.get("/", catchErrors(getAllTags));
 
 // if the user selects none of the sugesstions and add his custom ones
-router.post("/", addTags);
+router.post("/", catchErrors(addTags));
 
 
 module.exports = router;
